63a242ce11e63e083aace589dc27c590f9e6563c
Alex Zhu <alexyifanzhu@gmail.com>
2024-12-21 13:39:03 -0500

    Cycles are now multiples of 2pi

--- algorithm.py
+++ algorithm.py
@@ -4,13 +4,13 @@ import math
 # Given n points, we want to create a function as a sum of e^ikt to pass through all n points
 
 # Points will be denoted as an ordered pair, with its real part and imaginary part
-points=[[0,4],[2,4],[3,5]]
+points=[[0,4],[2,4],[3,5],[0,4]]
 
 aCoefficients=[]
 for i in range(len(points)):
     temp=[]
     for j in range(len(points)):
-        temp.append(math.cos(i*j))
+        temp.append(math.cos(i*j*math.pi/(2*len(points))))
     aCoefficients.append(temp)
 aCoefficients=np.array(aCoefficients)
 asolutions=np.array([point[0] for point in points])
@@ -21,7 +21,7 @@ bCoefficients=[]
 for i in range(len(points)):
     temp=[1]
     for j in range(1,len(points)):
-        temp.append(math.sin(i*j))
+        temp.append(math.sin(i*j*math.pi/(2*len(points))))
     bCoefficients.append(temp)
 bCoefficients=np.array(bCoefficients)
 bsolutions=np.array([point[1] for point in points])
