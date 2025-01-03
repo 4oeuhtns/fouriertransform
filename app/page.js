8d69c27f9e05072df506808180aa25b0fa8bc1be
Alex Zhu <alexyifanzhu@gmail.com>
2025-01-03 10:26:07 -0500

    Extra points

--- app/page.js
+++ app/page.js
@@ -37,16 +37,24 @@ export default function Home() {
   //   [-2, 1],
   //   [-2, 1.5],
   // ];
-
   let points=[
-    [0, 0], [2.0, 0], [4, 0], [6.0, 0], [8, 0], [8, 2.0], [8, 4], [8, 6.0], [8, 8], [6.0, 8], [4, 8], [2.0, 8], [0, 8], [0, 6.0], [0, 4], [0, 2.0]  ]
+    [0, 0], [1.0, 0], [2, 0], [3.0, 0], [4, 0], [4, 1.0], [4, 2], [4, 3.0], [4, 4], [3.0, 4], [2, 4], [1.0, 4], [0, 4], [0, 3.0], [0, 2], [0, 1.0]
+  ]
+
+  // let points =[
+  //   [0, 0], [1.0, 0], [2, 0]
+  // ]
+
+  // let points=[
+  //   [0, 0], [2.0, 0], [4, 0], [6.0, 0], [8, 0], [8, 2.0], [8, 4], [8, 6.0], [8, 8], [6.0, 8], [4, 8], [2.0, 8], [0, 8], [0, 6.0], [0, 4], [0, 2.0]  
+  // ]
   return (
     <div className="h-screen w-full flex justify-center items-center text-5xl font-operatorItalic">
       <h1 className="text-5xl font-operatorItalic">Fourier Transforms</h1>
       <p></p>
       <br>
       </br>
-      <Epicycle points={points} speed={0.01} width={600} height={600} />
+      <Epicycle points={points} speed={0.001} width={600} height={600} />
     </div>
   );
 }
