f7612bce44215ff65ff7fbe1d21914a913f97a49
Felix Zhao <zhaofelixdelt@gmail.com>
2025-01-06 13:11:15 -0500

    test

--- app/test/page.js
+++ app/test/page.js
@@ -6,17 +6,29 @@ import DrawingCanvas from "@/components/drawingCanvas";
 import { useState } from "react";
 
 export default function Test() {
-
+    let points = [
+        [
+          -119,
+          -105
+        ],
+        [
+          -119,
+          -105
+        ],
+        [
+          -18,
+          5
+        ],
+        [
+          82,
+          98
+        ]
+      ]
 
   return (
 <div className="h-screen w-full flex flex-col  items-center text-5xl font-operatorItalic">
 <h1 className="text-5xl font-operatorItalic">Fourier Transforms</h1>
-      {/* <div className="w-[600px] h-[600px]"><Epicycle points={points} speed={0.001} width={600} height={600}/></div> */}
-      <div><DrawingCanvas onPointsUpdate={handlePointsUpdate} width={400} height={600}></DrawingCanvas></div>
-      <div>
-        <h3>Points Received in Parent:</h3>
-        <pre>{JSON.stringify(points, null, 2)}</pre>
-      </div>
+      <div className="w-[600px] h-[600px]"><Epicycle points={points} speed={0.001} width={1000} height={1000}/></div>
      
     </div>
   );
